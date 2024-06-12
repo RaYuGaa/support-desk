efb9ed99a12f6efa97c9090f64d217c9fc9d8da8
Rayuga Nub <75535740+RaYuGaa@users.noreply.github.com>
2024-06-12 14:19:00 +0545

    Error & Exception Handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,26 @@
-const registerUser = (req, res) => {
-    res.send('Register Route')
+const asyncHandler = require('express-async-handler')
+
+// @desc Register a new user
+// @route /api/users
+// @access public
+const registerUser = asyncHandler(async (req, res) => {
+   const {name, email, password} = req.body
+
+// Validation
+if(!name || !email || !password){
+    res.status(400)
+    throw new Error('Please include all fields')
 }
 
-const loginUser = (req, res) => {
+    res.send('Register Route')
+})
+
+// @desc login a new user
+// @route /api/users/login
+// @access public
+const loginUser = asyncHandler(async (req, res) => {
     res.send('Login Route')
-}
+})
 
 module.exports = {
     registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errHandler, errorHandler} = require('./middleware/errorMiddleware')
 
 PORT = process.env.PORT || 5000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Welcome to support desk API'})
@@ -11,6 +15,8 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
