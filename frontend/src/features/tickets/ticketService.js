dc3254b497f1ab61471a023c8113c0a011038332
Rayuga Nub <75535740+RaYuGaa@users.noreply.github.com>
2024-06-18 17:17:10 +0545

    Single Ticket Display & Close Ticket Functionality

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api/tickets'
+const API_URL = 'http://localhost:5000/api/tickets/'
 
 // Create new ticket
 const createTicket = async (ticketData, token) => {
@@ -23,14 +23,42 @@ const getTickets = async (token) => {
         }
     }
 
-    const response = await axios.get(API_URL,config)
+    const response = await axios.get(API_URL, config)
+
+    return response.data
+}
+
+// Get user tickets
+const getTicket = async (ticketId, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.get(API_URL + ticketId , config)
+
+    return response.data
+}
+
+// Close ticket
+const closeTicket = async (ticketId, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.put(API_URL + ticketId , {status: 'closed'} , config)
 
     return response.data
 }
 
 const ticketService = {
     createTicket,
-    getTickets
+    getTickets,
+    getTicket,
+    closeTicket
 }
 
 export default ticketService
