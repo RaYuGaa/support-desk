1fa7d9ee9167c1fe80efbde3bbd9b66d9ae1b7f4
Rayuga Nub <75535740+RaYuGaa@users.noreply.github.com>
2024-06-19 16:18:40 +0545

    Submit a Note

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -5,8 +5,8 @@ function NoteItem({note}) {
 
   return (
     <div className="note" style={{
-        backgroundColor: note.isStaff ? 'rgba(,0,0,0,0.7)' : '#fff',
-        textColor: note.isStaff ? '#fff' : '#000',
+        backgroundColor: note.isStaff ? 'rgba(0,0,0,0.7)' : '#fff',
+        color: note.isStaff ? '#fff' : '#000',
     }}>
         <h4>Note from {note.isStaff ? <span>Staff</span> : <span>{user.name}</span>}</h4>
       <p>{note.text}</p>
